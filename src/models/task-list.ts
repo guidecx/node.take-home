cb10dd043535e9faf2c4cbd072218821d2b82952
Andre Araujo <araujo.a@me.com>
2021-02-09 16:12:05 -0700

    refactor: change TaskList model

--- src/models/task-list.ts
+++ src/models/task-list.ts
@@ -4,10 +4,10 @@ import { Task } from './task';
 export interface TaskList {
   id: number;
   name: string;
-  started_at: Date;
+  started_at: Date | null;
   due_date: Date;
   forecasted_completion_date: Date;
-  tasks: Task[];
+  tasks?: Task[];
   created_at: Date;
   updated_at: Date;
 }

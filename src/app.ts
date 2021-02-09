e750e17d3807e64a8f408855d56f616f9614d947
Andre Araujo <araujo.a@me.com>
2021-02-09 16:10:09 -0700

    chore: eslintrc rules

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,11 @@
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
 import compression from 'compression';
 import lusca from 'lusca';
 
 import * as settings from './config/settings';
-import { health, home } from './controllers/root';
-import * as taskList from './controllers/task-list';
+import { health, home } from '~/controllers/root';
+import * as taskList from '~/controllers/task-list';
+import AppError from './util/errors/AppError';
 
 const app = express();
 app.set('port', settings.port);
@@ -13,10 +14,25 @@ app.use(compression());
 
 app.use(lusca.xframe('SAMEORIGIN'));
 app.use(lusca.xssProtection(true));
+app.use(express.json());
 
 app.get('/', home);
 app.get('/health', health);
 
-app.get('/api/task-lists', taskList.list);
+app.get('/api/task-lists', taskList.index);
+
+app.use(function (err: Error, req: Request, res: Response, next: NextFunction) {
+  if (err instanceof AppError) {
+    return res
+      .status(err.statusCode)
+      .json({ status: 'error', message: err.message });
+  }
+
+  console.error(err);
+
+  return res
+    .status(500)
+    .json({ status: 'error', message: 'Internal server error' });
+});
 
 export default app;

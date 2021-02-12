4ac0d0e517900863f448f54e99ad2e99e0411aae
Andre Araujo <araujo.a@me.com>
2021-02-12 10:37:04 -0700

    fix: remove http test

--- tests/controllers/root/root.test.ts
+++
@@ -1,9 +0,0 @@
-import request from 'supertest';
-
-import app from '@/config/app';
-
-describe('GET /health', () => {
-  it('should return 200 OK', () => {
-    return request(app).get('/health').expect(200);
-  });
-});
